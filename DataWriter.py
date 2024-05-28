eec36524bb38085d5cf21f19ba1a89de5ab6f2ae
cholewakrzysztof <cholewa.krzysztof24@gmail.com>
2024-05-28 21:25:13 +0200

    ber,no coder, redundancy count

--- DataWriter.py
+++ DataWriter.py
@@ -13,30 +13,36 @@ class DataWriter:
         file_name = f'{path}\\{name}_{timestamp_str}.csv'
         file_csv = open(file_name, 'w', newline='')
         writer = csv.writer(file_csv, delimiter=';')
-        writer.writerow(['Coder', 'Channel', 'Packet Size', 'In', 'Out', 'Differences', 'Bit error rate'])
+        writer.writerow(['Coder',
+                         'Channel',
+                         'Packet Size',
+                         'In',
+                         'Out',
+                         'Differences',
+                         'Redundancy bit count',
+                         'Bit error rate'])
 
         coder = report.coder
         channel = report.channel
-        sum_differences = 0
         packet_size = report.packet_size
         error_bit_rate = report.error_bit_rate
-        print("Warning: hardcoded error_bit_rate")
+        redundancy_sum = report.redundancy
 
         for result in report.test_results:
             in_packet = result.in_bits
             out_packet = result.out_bits
             differences = result.differences_count
+            redundancy = result.redundancy_bits_count
 
-            sum_differences += differences
             writer.writerow([coder,
                              channel,
                              packet_size,
                              in_packet,
                              out_packet,
                              differences,
+                             redundancy,
                              ''])
 
-        writer.writerow([coder, channel, packet_size, '', '', '', error_bit_rate])
-        print(f"{coder};{channel};{packet_size};;;{error_bit_rate}")
+        writer.writerow([coder, channel, packet_size, '', '', '', redundancy_sum, error_bit_rate])
 
         print("Date been written into csv file" + file_name)
